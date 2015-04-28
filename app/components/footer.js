d237eeea5beb02f989ca3fc241153e7d8cf7741b
bobbykolev <bobbykolev@abv.bg>
2015-04-28 18:03:07 +0300

    react-router integrated

---
+++ app/components/footer.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function () {
+    return (
+    	<div className="jumbotron">
+      		<h3>footer!</h3>
+    	</div>
+    );
+  }
+});
--- app/main.js
+++ app/main.js
@@ -1,4 +1,39 @@
 var React = require('react');
 var Home = require('./components/home.js');
+var Login = require('./components/login.js');
+var Footer = require('./components/footer.js');
+var Router = require('react-router');
 
-React.render(<Home/>, document.getElementById('wrapper'));
+var DefaultRoute = Router.DefaultRoute;
+var Link = Router.Link;
+var Route = Router.Route;
+var RouteHandler = Router.RouteHandler;
+
+var App = React.createClass({
+  render: function () {
+    return (
+      <div id="wrapper">
+        <header>
+          <ul>
+            <li><Link to="home">Home</Link></li>
+            <li><Link to="login">login</Link></li>
+          </ul>
+          Logged in as Jane
+        </header>
+        <RouteHandler/>
+        <Footer/>
+      </div>
+    );
+  }
+});
+
+var routes = (
+  <Route name="home" path="/" handler={App}>
+    <Route name="login" handler={Login}/>
+    <DefaultRoute handler={Home}/>
+  </Route>
+);
+
+Router.run(routes, function (Handler) {
+  React.render(<Handler/>, document.body);
+});
